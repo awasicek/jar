7a91a5266f8e5d73f706fdbe82431528c69b903f
Andrew Wasicek <andrew.wasicek@gmail.com>
2016-02-22 16:42:41 -0800

    added ability to attach photos to a memory when creating a new memory; display thumbnail of a memory's associated photo next to the memory

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219195109) do
+ActiveRecord::Schema.define(version: 20160223002639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,10 +30,14 @@ ActiveRecord::Schema.define(version: 20160219195109) do
   create_table "mems", force: :cascade do |t|
     t.string   "date"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "jar_id"
     t.integer  "user_id"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "mems", ["jar_id"], name: "index_mems_on_jar_id", using: :btree
