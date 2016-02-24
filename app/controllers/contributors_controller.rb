c4959998a7202bc360dcf189c7939986a745b625
Andrew Wasicek <andrew.wasicek@gmail.com>
2016-02-24 15:06:21 -0800

    started styling the sign up page

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -14,6 +14,10 @@ class ContributorsController < ApplicationController
     @jar = Jar.find(params[:jar_id])
     @user = User.find_by({username: params[:user][:username]})
     @contributor = @jar.contributors.new({user: @user, jar: @jar})
+    if @contributor.user == nil
+      flash[:failure] = "Failed to add contributor."
+      redirect_to new_contributor_path
+    end
     if @contributor.save
       flash[:success] = "You successfully added #{@user.username} as a contributor."
       redirect_to users_path
